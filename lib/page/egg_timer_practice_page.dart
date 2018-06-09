9a89bc7801c0170270b36a3a7bcb822311d8450e
任志彬 <renzb@51weker.cn>
2018-06-09 15:12:46 +0800

    TimerPractice

--- lib/page/egg_timer_practice_page.dart
+++ lib/page/egg_timer_practice_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 final Color GRADIENT_TOP = const Color(0xfff5f5f5);
@@ -62,46 +64,188 @@ class TimeDial extends StatelessWidget {
                 ]),
             child: new Container(
               width: double.infinity,
-              child: new Padding(
-                padding: const EdgeInsets.all(65.0),
-                child: new AspectRatio(
-                  aspectRatio: 1.0,
-                  child: new Container(
+              child: new Stack(
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(65.0),
+                    child: new TimerKnob(),
+                  ),
+                  new Container(
+                    padding: const EdgeInsets.all(55.0),
                     width: double.infinity,
-                    decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        gradient: new LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [GRADIENT_TOP, GRADIENT_BOTTOM]),
-                        boxShadow: [
-                          new BoxShadow(
-                              color: const Color(0x44000000),
-                              blurRadius: 2.0,
-                              spreadRadius: 1.0,
-                              offset: const Offset(0.0, 1.0))
-                        ]),
-                    child: new Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: new Container(
-                        decoration: new BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: new Border.all(
-                                width: 1.5, color: const Color(0xffdfdfdf))),
-                        child: new Icon(
-                          Icons.access_time,
-                          size: 50.0,
-                        ),
-                      ),
+                    height: double.infinity,
+                    child: new CustomPaint(
+                      painter: new TimeTicker(),
                     ),
                   ),
-                ),
+                ],
               ),
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class TimeTicker extends CustomPainter {
+  final LONG_TICK = 14.0;
+  final SHORT_TICK = 4.0;
+
+  final tickCount;
+  final tickPerSection;
+  final tickInset;
+  Paint tickPainter;
+  TextPainter textPainter;
+  TextStyle textStyle;
+
+  TimeTicker(
+      {this.tickCount = 35, this.tickPerSection = 5, this.tickInset = 0.0})
+      : tickPainter = new Paint(),
+        textPainter = new TextPainter(
+            textAlign: TextAlign.center, textDirection: TextDirection.ltr),
+        textStyle = new TextStyle(
+            color: Colors.black, fontSize: 20.0, fontFamily: "BebasNeue") {
+    tickPainter.style = PaintingStyle.stroke;
+    tickPainter.strokeWidth = 1.5;
+    tickPainter.color = Colors.black;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.save();
+    canvas.translate(size.width / 2, size.height / 2);
+    canvas.save();
+    double radius = size.height / 2;
+    for (int i = 0; i < tickCount; i++) {
+      double tickLength = i % tickPerSection == 0 ? LONG_TICK : SHORT_TICK;
+      canvas.drawLine(new Offset(0.0, -radius),
+          new Offset(0.0, -tickLength - radius), tickPainter);
+
+      if (i % tickPerSection == 0) {
+        canvas.save();
+        canvas.translate(0.0, -radius - 30);
+        textPainter.text = new TextSpan(text: "$i", style: textStyle);
+        textPainter.layout();
+        var tickPercent = i / tickCount;
+        var quadrant;
+        if (tickPercent < 0.25) {
+          quadrant = 1;
+        } else if (tickPercent < 0.5) {
+          quadrant = 4;
+        } else if (tickPercent < 0.75) {
+          quadrant = 3;
+        } else {
+          quadrant = 2;
+        }
+        switch (quadrant) {
+          case 1:
+            break;
+          case 4:
+            canvas.rotate(-pi / 2);
+            break;
+          case 2:
+            break;
+          case 3:
+            canvas.rotate(pi / 2);
+            break;
+        }
+        textPainter.paint(canvas,
+            new Offset(-textPainter.width / 2, -textPainter.height / 2));
+        canvas.restore();
+      }
+      canvas.rotate(2 * pi / tickCount);
+    }
+
+    canvas.restore();
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class TimerKnob extends StatelessWidget {
+  const TimerKnob({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[
+        new Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: new CustomPaint(
+            painter: new ArrowPainter(),
+          ),
+        ),
+        new AspectRatio(
+          aspectRatio: 1.0,
+          child: new Container(
+            width: double.infinity,
+            decoration: new BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: new LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [GRADIENT_TOP, GRADIENT_BOTTOM]),
+                boxShadow: [
+                  new BoxShadow(
+                      color: const Color(0x44000000),
+                      blurRadius: 2.0,
+                      spreadRadius: 1.0,
+                      offset: const Offset(0.0, 1.0))
+                ]),
+            child: new Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: new Container(
+                decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: new Border.all(
+                        width: 1.5, color: const Color(0xffdfdfdf))),
+                child: new Icon(
+                  Icons.access_time,
+                  size: 50.0,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ArrowPainter extends CustomPainter {
+  Paint _dialArrowPaint;
+
+  ArrowPainter() {
+    _dialArrowPaint = new Paint();
+    _dialArrowPaint.color = Colors.black;
+    _dialArrowPaint.style = PaintingStyle.fill;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.save();
+    canvas.translate(size.width / 2, 0.0);
+    var path = new Path();
+    path.moveTo(0.0, -10.0);
+    path.lineTo(5.0, 0.0);
+    path.lineTo(-5.0, 0.0);
+    path.close();
+    canvas.drawPath(path, _dialArrowPaint);
+    canvas.drawShadow(path, Colors.black, 3.0, false);
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
 
